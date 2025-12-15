58a3bf6eb8524f026f4797a24334e4c91331c459
Arnav2007-D <arnav.payal77@gmail.com>
2025-12-15 12:49:52 +0800

    Refactor city list and clean up code
    Updated city names in the guessing game and removed emojis.

--- ARNAV CITY GUESS GAME/City_Guesser2.py
+++ ARNAV CITY GUESS GAME/City_Guesser2.py
@@ -1,9 +1,9 @@
 import random
 
 def choose_city():
-    cities = ['bangkok 🗼', 'london 🏰','mecca 🕌', 'dubai 🏙️', 'paris 🗼',
-    'tokyo 🗼', 'rome 🏛️', 'milan 👠','new york 🗽', 'amsterdam 🚲', 
-    'sydney 🏖️', 'singapore 🌆', 'barcelona 🎨']
+    cities = ['bangkok', 'london', 'mecca', 'dubai', 'paris',
+              'tokyo', 'rome', 'milan', 'nagasaki', 'amsterdam', 
+              'sydney', 'singapore', 'barcelona']
     return random.choice(cities) 
 
 
@@ -58,3 +58,4 @@ def city_guessing_game():
         input("Press Enter to exit the game...")
 
 city_guessing_game()
+
